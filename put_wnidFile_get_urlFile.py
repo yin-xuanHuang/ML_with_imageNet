8085c141505cb9f2c3a70efe902d6020ad93d38b
yinxuanh <yinxuanh@gmail.com>
2017-09-25 20:51:27 +0800

    Add wnidFile2urlFile

---
+++ put_wnidFile_get_urlFile.py
@@ -0,0 +1,128 @@
+import os
+from random import shuffle
+from psutil import virtual_memory
+import time
+
+
+def main():
+    # 未避免記憶體塞暴，每隔 1024*256 強制清空一次 lists
+    max_lines = 1024*256
+    # 紀錄開始時間
+    startTime = time.time()
+    shuffle_data = True  # 打亂順序
+    # 查看urls資料夾里有沒有可能的url_file
+    urls_dirPath = os.path.join("urls")
+    urls_dirList = list()
+    for d in os.listdir(urls_dirPath):
+        if os.path.isfile(os.path.join(urls_dirPath, d)):
+            if d != "readme":
+                urls_dirList.append(d)
+    # 假如有新檔案，就假設為url file
+    if not len(urls_dirList):
+        print("urls 資料夾內沒有url_file，請閱讀urls/readme，自行下載檔案。")
+    else:
+        # 列出可能有效的url file
+        print("url file(s): {}".format("|".join(urls_dirList)), end=" ")
+        print()
+        # 過濾篩選可能有效的資料夾
+        dirList = list()
+        for d in os.listdir():
+            if os.path.isdir(os.path.join("", d)):
+                if d != "urls" and d != "words":
+                    dirList.append(d)
+
+        if not len(dirList):
+            print("沒有有效資料夾。")
+        else:
+            while True:
+                # 列出有效資料夾
+                for idx, value in enumerate(dirList):
+                    print("[{}]:{}".format(idx, value))
+                # 使用者輸入
+                dirIdex = input("請選擇內含wnid_file的資料夾(ex.:0):")
+                if not dirIdex.isnumeric():
+                    print("請輸入自然數！！")
+                elif int(dirIdex) > len(dirList) - 1:
+                    print("請輸入有效數字！！")
+                else:
+                    # 工作資料夾
+                    dirPath = os.path.join(dirList[int(dirIdex)])
+                    # 確認是否有 wnidFile
+                    if not os.path.isfile(os.path.join(dirPath,
+                                                       dirList[int(dirIdex)] + "_wnids")):
+                        print("此 {} 資料夾內，沒有wnidFile！！".format(dirList[int(dirIdex)]))
+                        break
+                    else:
+                        # 將檔案內的 wnid 以列為單位，讀到 list 內
+                        wnidList = list()
+                        with open(os.path.join(dirPath,dirList[int(dirIdex)] + "_wnids"), "r") as f:
+                            wnidList = f.readlines()
+                        f.close()
+
+                        # 所有 wnid 所對應的 urls
+                        wnidList2urlsList = list()
+                        # 計數對應到的 line
+                        count = 0
+                        count0 = 0
+                        count1 = 0
+                        not_match_list = list()
+                        # 將 url_file 有對應到 wnid 的 line 都存在 wnidList2urlsList
+                        for url_file in urls_dirList:
+                            with open(os.path.join(urls_dirPath, url_file), mode="r", errors="ignore") as f:
+                                lineList = f.readlines()
+                            f.close()
+
+                            for line in lineList:
+                                list_wnidid_url = line.split("\t")
+                                wnid = list_wnidid_url[0].split("_")[0]
+                                if wnid + "\n" in wnidList:
+                                    wnidList2urlsList.append(line)
+                                    count += 1
+                                    count1 += 1
+                                else:
+                                    not_match_list.append(line)
+                                    count0 += 1
+                                if (count1 + count0) / max_lines >= 1:
+                                    mem = virtual_memory()
+                                    print("free memory: {} ".format(mem.free))
+                                    if count1 == 0:
+                                        # y=0 順序隨機調換
+                                        if shuffle_data:
+                                            shuffle(not_match_list)
+                                        # 寫入檔案 y=0
+                                        with open(os.path.join(dirPath, dirList[int(dirIdex)] + "_urls_0"), "a+") as f0:
+                                            for line in not_match_list[:100]:
+                                                f0.write(line)
+                                        f0.close()
+                                        not_match_list = list()
+                                    else:
+                                        # 寫入檔案 y=1
+                                        with open(os.path.join(dirPath, dirList[int(dirIdex)] + "_urls_1"), "a+") as f1:
+                                            for line in wnidList2urlsList:
+                                                f1.write(line)
+                                        f1.close()
+                                        wnidList2urlsList = list()
+                                        # y=0 順序隨機調換
+                                        if shuffle_data:
+                                            shuffle(not_match_list)
+                                        # 寫入檔案 y=0
+                                        with open(os.path.join(dirPath, dirList[int(dirIdex)] + "_urls_0"), "a+") as f0:
+                                            if count0 < count1 * 3 + 1:
+                                                for line in not_match_list:
+                                                    f0.write(line)
+                                            else:
+                                                for line in not_match_list[:count1 * 3 + 1]:
+                                                    f0.write(line)
+                                        f0.close()
+                                        not_match_list = list()
+                                    count0 = 0
+                                    count1 = 0
+
+                        print("Y=1, count= {} ".format(count))
+                        print("work get done!")
+                        print("Cost time= {} ".format(time.time() - startTime))
+                        break
+
+
+if __name__ == "__main__":
+    main()
